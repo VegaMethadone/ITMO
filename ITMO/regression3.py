3789b07bc36c8178d6bdaab402f59eae0adfa763
Tema <92046258+VegaMethadone@users.noreply.github.com>
2023-03-09 21:42:56 +0300

    Delete regression3.py

--- ITMO/regression3.py
+++
@@ -1,9 +0,0 @@
-x = 0
-summ = 0
-for i in range(1033, 7737):
-    if (i % 5 == 0) and (i % 11 != 0) and (i % 17 != 0) and (i % 19 != 0) and (i % 23 != 0):
-        x += 1
-        if summ < i:
-            summ = i
-            print(x)
-            print(summ)
