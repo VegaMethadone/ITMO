62733714ab2a7cbc32e34c2f0bc9ffc2a4ab3e29
Tema <92046258+VegaMethadone@users.noreply.github.com>
2023-04-15 21:42:01 +0300

    Update and rename test.py to parsing.py

--- architecture of AI systems/Parsing data/parsing.py
+++ architecture of AI systems/Parsing data/parsing.py
@@ -12,7 +12,6 @@ def mkdir_own():
     os.mkdir('./assets')
 
 
-
 def parse_article():
 
     with open("links.json") as link:
@@ -28,15 +27,30 @@ def parse_article():
         soup = BeautifulSoup(response.content, "html.parser")
 
         article_text = ""
-        for p in soup.find_all("div", class_="article-formatted-body"):
-            article_text += p.text.strip()
+        tegs = []
+        habs = []
+
+        
+
+        for t in soup.find_all("div", class_="article-formatted-body"):
+            article_text += t.text.strip()
+        
+        for p in soup.find_all("a", class_="tm-tags-list__link"):
+            tegs.append(p.text.strip())
+
+        for p in soup.find_all("a", class_="tm-hubs-list__link"):
+            habs.append(p.text.strip())
+
+        jsonData = {
+          "article_text":  article_text, 
+          "tegs": tegs,
+          "habs": habs,
+        }
 
         data = {f"article{i}": article_text}
         with open(f"article{i}.json", "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
+            json.dump(jsonData,  f, ensure_ascii=False, indent=2)
         i += 1
-
-
 
 def main():
     parse_article()
